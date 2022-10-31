2cfba48d6216846c8dfa67ccfbc23a7abf86c76d
Ferrari-coder <oluwaferanmiaiyegbusi@gmail.com>
2022-10-31 17:28:06 +0100

    done

--- src/page.js
+++ src/page.js
@@ -3,9 +3,9 @@ import Profileimage from "./images/profileimg.png";
 import slack from "./images/slack.png";
 import github from "./images/Icon.png";
 import ingressive from "./images/I4G.png";
-import share from "./images/share.png"
+import share from "./images/share.png";
 import sharemobile from "./images/mobileshare.png";
-import "./style.css"
+import "./style.css";
 
 const PageContainer = () => {
   const buttonTitles = [
@@ -44,35 +44,40 @@ const PageContainer = () => {
 
   const buttonList = buttonTitles.map((item) => {
     return (
-     
-        <a href={item.url}>
-          <button id={item.iden}>{item.title}</button>
-        </a>
-      
+      <a href={item.url}>
+        <button id={item.iden}>{item.title}</button>
+      </a>
     );
   });
   return (
     <>
       <div className="container">
-        <img src={share} alt="share button" className="share"/>
+        <div className="share-grid">
+          <div></div>
+          <div></div>
+          <div></div>
+          <img src={share} alt="share button" className="share" />
         <img src={sharemobile} alt="share-mobile" className="share-mobile" />
+        </div>
         <div className="profile-part">
           <img src={Profileimage} alt="profilepic" id="profile__img" />
           <p id="twitter">Ahm_Tamilore</p>
           <p id="slack">Aiyegbusi Oluwaferanmi</p>
         </div>
-        <div className="all-buttons">
-            {buttonList}
-        </div>
+        <div className="all-buttons">{buttonList}</div>
         <div className="icons">
-            <img src={slack} alt="slack-icon" />
-            <a href="https://github.com/Ferrari-coder"><img src={github} alt="github-icon" /></a>
+          <img src={slack} alt="slack-icon" />
+          <a href="https://github.com/Ferrari-coder">
+            <img src={github} alt="github-icon" />
+          </a>
         </div>
-            <div className="underline"></div>
+        <div className="underline"></div>
         <footer>
-            <h2>Zuri<span>.</span>Internship</h2>
-            <p>HNG Internship 9 Frontend Task</p>
-            <img src={ingressive} alt="I4G-logo" />
+          <h2>
+            Zuri<span>.</span>Internship
+          </h2>
+          <p>HNG Internship 9 Frontend Task</p>
+          <img src={ingressive} alt="I4G-logo" />
         </footer>
       </div>
     </>
